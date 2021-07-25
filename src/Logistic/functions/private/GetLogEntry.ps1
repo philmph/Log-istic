dbe45e8a3952ba3653d79d17cc7460992dc9aa9f
Philipp Maier <git@pmaier.at>
2021-07-25 21:11:56 +0200

    Initial internal function GetLogentry logic

--- src/Logistic/functions/private/GetLogEntry.ps1
+++ src/Logistic/functions/private/GetLogEntry.ps1
@@ -1,2 +1,42 @@
-# TODO: Called from ConvertTo-LogEntry and Write-Log
-# TODO: String output back to calling function
+function GetLogentry {
+    [CmdletBinding()]
+
+    param (
+        # Message, Type,
+    )
+
+    begin {
+        Set-StrictMode -Version 3
+    }
+
+    process {
+        $Date = Get-Date -Format 'yyyy-MM-dd\ HH\:mm\:ss\.fff'
+
+        if ($MyInvocation.PSCommandPath) {
+            $File = Split-Path -Path $MyInvocation.PSCommandPath -Leaf
+        } else {
+            $File = $MyInvocation.CommandOrigin
+        }
+
+        # TODO: Rework to split JSON / SCCM
+
+        if ($JSONFormat) {
+            $Data = [PSCustomObject] @{
+                Date = (Get-Date )
+                Type = $Type
+                Data = $InputObject
+            } | ConvertTo-Json -Compress
+        }
+
+        if ($SCCMFormat) {
+
+
+            $Line = '<![LOG[{0}]LOG]!><time="{1:HH\:mm\:ss\.ffffff}" date="{1:MM-dd-yyyy}" component="{2}" context="" type="{3}" thread="" file="{2}">' -f `
+            $Message, $Date, $File, $TypeShort
+        }
+
+        # TODO: Return String
+    }
+
+    end {}
+}
