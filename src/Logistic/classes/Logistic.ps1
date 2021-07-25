6f81920250455f5e68f05604722d7e0a790a7acf
Philipp Maier <git@pmaier.at>
2021-07-25 11:11:42 +0200

    Add initial Logistics class

--- src/Logistic/classes/Logistic.ps1
+++ src/Logistic/classes/Logistic.ps1
@@ -3,29 +3,10 @@ class Logistic
     [LogisticType]$Type
     [LogisticFormat]$Format
     [string]$Path
+    hidden [System.IO.StreamWriter]$StreamWriter
 
     # Constructor with overloads
     Logistic ([LogisticType]$Type, [LogisticFormat]$Format, [string]$Path) {
-        <#
-        # ! Needs rework
-        # Initialize StreamWriter if neccessary
-        if ($this.Type -eq [LogisticType]::StreamWriter) {
-            if (Test-Path -Path $this.Path -PathType Leaf) {
-                $Fullname = (Get-Item -Path $Path).FullName
-            } elseif ((-not (Test-Path -Path $this.Path -PathType Container)) -and (Test-Path -Path $this.Path -IsValid)) {
-                $Fullname = (New-Item -Path $Path -ItemType File -Force).Fullname
-            } else {
-                throw "Cannot initialize StreamWriter for $Path"
-            }
-
-            try {
-                $this.StreamWriter = [System.IO.StreamWriter]::new($FullName, [System.Text.Encoding]::UTF8)
-            } catch {
-                throw "Cannot initialize StreamWriter for $Path"
-            }
-        }
-        #>
-
         $this.Type = $Type
         $this.Format = $Format
         $this.Path = $Path
