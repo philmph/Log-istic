1e7d6e46a3fb632781de0c9079ed5140ac55e7e3
Philipp Maier <git@pmaier.at>
2021-07-25 18:55:33 +0200

    Update class with StreamWriter init

--- src/Logistic/classes/Logistic.ps1
+++ src/Logistic/classes/Logistic.ps1
@@ -1,44 +1,39 @@
 class Logistic
 {
+    [string]$Path
+    hidden [string]$Fullpath
+
     [LogisticType]$Type
     [LogisticFormat]$Format
-    [string]$Path
 
-    hidden [string]$Fullname
     hidden [System.IO.StreamWriter]$StreamWriter
 
     # Hidden init methods for constructor use
     hidden Init ([string]$Path) {
         # Set Outfile as default
-        $this.Init('Outfile', $Path)
+        $this.Init($Path, 'Outfile', 'JSON')
     }
 
-    hidden Init ([LogisticType]$Type, [string]$Path) {
-        # TODO: Logic snippet for when SQL or WindowsEventlog is added
-        # if ($Type -in @([LogisticType]::Outfile, [LogisticType]::StreamWriter)) {}
-
-        # Set JSON as default
-        $this.Init($Type, 'JSON', $Path)
-    }
-
-    hidden Init([LogisticType]$Type, [LogisticFormat]$Format, [string]$Path) {
-        if ((Test-Path -Path $Path -PathType 'Container') -or (-not (Test-Path -Path $Path -IsValid))) {
-            throw "$Path is not valid"
-        }
-
-        # TODO: Evaluate Upper or Lower
-
-        if (-not (Test-Path -Path $Path -PathType Leaf)) {
-            $Fullname = (New-Item -Path $Path -ItemType File -Force).Fullname
-        } elseif ((-not (Test-Path -Path $Path -PathType Container)) -and (Test-Path -Path $Path -IsValid)) {
-            $Fullname = (Get-Item -Path $Path).Fullname
+    hidden Init([string]$Path, [LogisticType]$Type, [LogisticFormat]$Format) {
+        if (-not (Test-Path -Path $Path -PathType Container) -and (Test-Path -Path $Path -IsValid)) {
+            if (Test-Path -Path $Path -PathType Leaf) {
+                $Fullname = (Get-Item -Path $Path).Fullname
+            } else {
+                $Fullname = (New-Item -Path $Path -ItemType File -Force).Fullname
+            }
         } else {
             throw "$Path is not valid"
         }
 
+        $this.Path = $Path
+        $this.Fullpath = $Fullname
+
         $this.Type = $Type
         $this.Format = $Format
-        $this.Path = $Path
+
+        if ($Type -eq [LogisticType]::StreamWriter) {
+            $this.InitializeStreamWriter()
+        }
     }
 
     # Constructor with overloads
@@ -46,33 +41,17 @@ class Logistic
         $this.Init($Path)
     }
 
-    Logistic ([LogisticType]$Type, [string]$Path) {
-        $this.Init($Type, $Path)
-    }
-
     # Constructor with overloads
-    Logistic ([LogisticType]$Type, [LogisticFormat]$Format, [string]$Path) {
-        $this.Init($Type, $Format, $Path)
+    Logistic ([string]$Path, [LogisticType]$Type, [LogisticFormat]$Format) {
+        $this.Init($Path, $Type, $Format)
     }
 
-    [System.IO.StreamWriter] InitializeStreamWriter () {
-        # TODO: Required here?
-
-        if (-not (Test-Path -Path $this.Path -PathType Leaf)) {
-            $Fullname = (New-Item -Path $this.Path -ItemType File -Force).Fullname
-        } elseif ((-not (Test-Path -Path $this.Path -PathType Container)) -and (Test-Path -Path $this.Path -IsValid)) {
-            $Fullname = (Get-Item -Path $this.Path).Fullname
-        } else {
-            throw "Cannot initialize StreamWriter for $($Fullname)"
-        }
-
+    [void] InitializeStreamWriter () {
         try {
-            $this.StreamWriter = [System.IO.StreamWriter]::new($Fullname, [System.Text.Encoding]::UTF8)
+            $this.StreamWriter = [System.IO.StreamWriter]::new($this.Fullpath, [System.Text.Encoding]::UTF8)
         } catch {
-            throw "Cannot initialize StreamWriter for $($Fullname): $_"
-            return $null
+            throw "Cannot initialize StreamWriter for $($this.Fullpath): $_"
         }
-        return $this.StreamWriter
     }
 
     [void] CloseStreamWriter () {
