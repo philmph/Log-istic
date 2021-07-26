4fd98f90afe7c3dc2c54d510ba7db5fc98117a3c
Philipp Maier <git@pmaier.at>
2021-07-26 17:19:09 +0200

    Add initial GetLogEntry logic

--- src/Logistic/functions/private/GetLogEntry.ps1
+++ src/Logistic/functions/private/GetLogEntry.ps1
@@ -1,42 +1,49 @@
 function GetLogentry {
     [CmdletBinding()]
 
+    # Data is verified in public funtions
     param (
-        # Format, Message, Type,
+        [Parameter(Mandatory)]
+        [string]$Format,
+
+        [Parameter(Mandatory)]
+        [psobject]$InputObject,
+
+        [Parameter(Mandatory)]
+        [string]$Type
     )
 
-    begin {
-        Set-StrictMode -Version 3
+    $Timestamp = Get-Date
+
+    if ($MyInvocation.PSCommandPath) {
+        $CallOrigin = Split-Path -Path $MyInvocation.PSCommandPath -Leaf
+    } else {
+        $CallOrigin = $MyInvocation.CommandOrigin
     }
 
-    process {
-        $Date = Get-Date -Format 'yyyy-MM-dd\ HH\:mm\:ss\.fff'
-
-        if ($MyInvocation.PSCommandPath) {
-            $File = Split-Path -Path $MyInvocation.PSCommandPath -Leaf
-        } else {
-            $File = $MyInvocation.CommandOrigin
-        }
-
-        # TODO: Rework to split JSON / SCCM
-
-        if ($JSONFormat) {
-            $Data = [PSCustomObject] @{
-                Date = (Get-Date )
-                Type = $Type
+    switch ($Format) {
+        'JSON' {
+            $Output = [PSCustomObject] @{
+                Timestamp = $Timestamp.ToString('yyyy-MM-dd\ HH\:mm\:ss\.fff')
+                CallOrigin = $CallOrigin
                 Data = $InputObject
+                Type = $Type
             } | ConvertTo-Json -Compress
         }
 
-        if ($SCCMFormat) {
+        'SCCM' {
+            $TypeShort = switch ($Type) {
+                'Verbose' { '1' }
+                'Warning' { '2' }
+                'Error' { '3' }
+            }
 
-
-            $Line = '<![LOG[{0}]LOG]!><time="{1:HH\:mm\:ss\.ffffff}" date="{1:MM-dd-yyyy}" component="{2}" context="" type="{3}" thread="" file="{2}">' -f `
-            $Message, $Date, $File, $TypeShort
+            $Output = '<![LOG[{0}]LOG]!><time="{1:HH\:mm\:ss\.ffffff}" date="{1:MM-dd-yyyy}" component="{2}" context="" type="{3}" thread="" file="{2}">' -f
+            $Message, $Timestamp, $File, $TypeShort
         }
 
-        # TODO: Return String
+        # We should never get here, since data is tested in public functions
+        default { throw 'Format for logentry creation unknown' }
     }
-
-    end {}
+    Write-Output -InputObject $Output
 }
