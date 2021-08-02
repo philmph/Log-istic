530b0f1e93a3fac8b60fb1af022dab6a964268a9
Philipp Maier <git@pmaier.at>
2021-08-02 20:35:53 +0200

    Add Write-Logentry documentation

--- src/Logistic/functions/public/Write-Logentry.ps1
+++ src/Logistic/functions/public/Write-Logentry.ps1
@@ -1,4 +1,53 @@
 function Write-Logentry {
+    <#
+    .SYNOPSIS
+        Writes log entries into logfile with specific formatting.
+
+    .DESCRIPTION
+        Writes log entries into logfile with specific formatting.
+
+    .PARAMETER LogisticObject
+        Required as this object provides required log settings.
+
+    .PARAMETER InputObject
+        Defines the main log information data. Can be [string] or complex formats like [PSCustomObject].
+
+    .PARAMETER Type
+        Defines the log type. Can be 'Verbose' (default), 'Warning' or 'Error'.
+
+    .EXAMPLE
+        C:\> Write-Logentry -LogisticObject $Logistic -InputObject 'Teststring' -Type Warning
+
+        WARNING: Teststring
+
+        # Content of logistic.log
+        C:\> Get-Content .\logistic.log
+        {"Timestamp":"2021-08-02 20:37:59.548","Callstack":"Runspace","Data":"Teststring","Type":"Warning"}
+
+    .EXAMPLE
+        C:\> Write-Logentry -LogisticObject $Logistic -InputObject ([PSCustomObject]@{Testobject = 'Data'}) -Type Error
+
+        Write-Logentry : @{Testobject=Data}
+
+        # Content of logistic.log
+        C:\> Get-Content .\logistic.log
+        {"Timestamp":"2021-08-02 20:39:57.247","Callstack":"Runspace","Data":{"Testobject":"Data"},"Type":"Error"}
+
+    .INPUTS
+        [string]
+
+    .OUTPUTS
+        -
+
+    .NOTES
+        Author: Philipp Maier
+        Author Git: https://github.com/philmph
+
+    .LINK
+        https://github.com/philmph/Log-istic/blob/main/docs/Write-Logentry.md
+    #>
+
+
     [CmdletBinding(SupportsShouldProcess)]
 
     param (
@@ -17,9 +66,7 @@ function Write-Logentry {
 
         [Parameter()]
         [ValidateSet('Verbose', 'Warning', 'Error')]
-        [string]$Type = 'Verbose',
-
-        [switch]$NoConsoleOutput
+        [string]$Type = 'Verbose'
     )
 
     begin {
@@ -30,11 +77,7 @@ function Write-Logentry {
         # Outputting data without formatting to keep it human readable
         $ConsoleOutput = $InputObject -as [string]
         switch ($Type) {
-            'Verbose' {
-                if (-not ($NoConsoleOutput)) {
-                    Write-Verbose -Message $ConsoleOutput
-                }
-            }
+            'Verbose' { Write-Verbose -Message $ConsoleOutput }
             'Warning' { Write-Warning -Message $ConsoleOutput }
             'Error' { Write-Error -Message $ConsoleOutput }
         }
