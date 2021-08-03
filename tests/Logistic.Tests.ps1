eb57020fefeb319ef52f1e0c412649628ae0995a
Philipp Maier <git@pmaier.at>
2021-08-03 21:20:33 +0200

    Update function tests

--- tests/Logistic.Tests.ps1
+++ tests/Logistic.Tests.ps1
@@ -4,7 +4,6 @@ Describe 'Module Tests' {
     $ModuleRoot = Resolve-Path -Path "$TestRoot\..\src\$ModuleName"
 
     Context 'Module and manifest tests' {
-        # Required in Pester v5 syntax
         $PSDefaultParameterValues = @{
             "It:TestCases" = @{
                 ModuleName = $ModuleName
@@ -21,7 +20,9 @@ Describe 'Module Tests' {
         }
 
         It -Name "<ModuleName>.psd1 passes Test-ModuleManifest" {
-            [bool](Test-ModuleManifest -Path "$ModuleRoot\$ModuleName.psd1") | Should -Be $true
+            $Result = [bool](Test-ModuleManifest -Path "$ModuleRoot\$ModuleName.psd1")
+
+            $Result | Should -Be $true
         }
     }
 
@@ -34,25 +35,78 @@ Describe 'Module Tests' {
         $Rules = (. $PSScriptAnalyzerSettings)["IncludeRules"]
 
         # Generate TestCases
-        $TestCases = @(
+        $TestCasesPSScriptAnalyzer = @(
             foreach ($Function in $Functions) {
                 $Result = @(Invoke-ScriptAnalyzer -Path $Function.FullName -Settings $PSScriptAnalyzerSettings)
 
                 foreach ($Rule in $Rules) {
                     @{
-                        Rule = $Rule
                         Name = $Function.Name
                         Result = $Result
+                        Rule = $Rule
                     }
                 }
             }
         )
 
-        It -Name "<Name> passes rule <Rule>" -TestCases $TestCases {
+        It -Name "<Name> passes rule <Rule>" -TestCases $TestCasesPSScriptAnalyzer {
             $Result |
-            Where-Object { $Rule -eq $_.RuleName } |
+            Where-Object { $_.RuleName -eq $Rule} |
             Select-Object -ExpandProperty Message |
             Should -BeNullOrEmpty
+        }
+
+        $TestCasesFunctionNaming = @(
+            foreach ($Function in $Functions) {
+                switch -regex ($Function.FullName) {
+                    '\\Private\\' {
+                        $Type = 'Private'
+                        $Pattern = '^\s*[Ff]unction\s+(([A-Z]{1,4}[a-z]+){1,2}([A-Z]{1,4}[a-z\d]+)+)(\s+\{)?$'
+                    }
+
+                    '\\Public\\' {
+                        $Type = 'Public'
+                        $Pattern = '^\s*[Ff]unction\s+(([A-Z]{1,4}[a-z]+){1,2}\-([A-Z]{1,4}[a-z\d]+)+)(\s+\{)?$'
+                    }
+                }
+
+                @{
+                    BaseName = $Function.BaseName
+                    FullName = $Function.FullName
+                    Name = $Function.Name
+                    Type = $Type
+                    Pattern = $Pattern
+                }
+            }
+        )
+
+        $PSDefaultParameterValues = @{
+            "It:TestCases" = $TestCasesFunctionNaming
+        }
+
+        It -Name "<Name> contains one function" {
+            $Result = @(Select-String -Path $FullName -Pattern '^\s*Function\s+')
+
+            $Result.Count | Should -Be 1
+        }
+
+        It -Name "<Name> should fit naming standards for <type> functions" {
+            $Result = @(Select-String -Path $FullName -Pattern $Pattern -CaseSensitive)
+
+            $Result.Count | Should -Be 1
+        }
+
+        It -Name "<Name> should match ps1 file name" {
+            $FunctionMatches = @(Select-String -Path $FullName -Pattern $Pattern -CaseSensitive)
+            $Result = $false
+
+            if ($FunctionMatches.Count -gt 0) {
+                if ($FunctionMatches.Matches[0].Groups[1].Value -ceq $BaseName) {
+                    $Result = $true
+                }
+            }
+
+            $Result | Should -Be $true
         }
     }
 }
