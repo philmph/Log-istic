8726c55bf912b5f4aac4719846f61825528620b2
Philipp Maier <git@pmaier.at>
2021-08-03 08:55:59 +0200

    Complete tests overhaul for Pester v5+

--- tests/Predefined.tests.ps1
+++ tests/Predefined.tests.ps1
@@ -1,35 +1,42 @@
-$ModuleName = "Logistic"
-$TestRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-$ModuleRoot = Resolve-Path -Path "$TestRoot\..\src\$ModuleName"
+
 
 Describe 'Module Tests' {
-    Context "Testing module $ModuleName" {
-        It 'has a .psm1 file' {
+    $ModuleName = "Logistic"
+    $TestRoot = $PSScriptRoot
+    $ModuleRoot = Resolve-Path -Path "$TestRoot\..\src\$ModuleName"
+    $Functions = Get-ChildItem "$ModuleRoot\functions" -Recurse -Filter *.ps1
+
+    Context 'Module and manifest tests' {
+        # Required in Pester v5 syntax
+        $PSDefaultParameterValues = @{
+            "It:TestCases" = @{
+                ModuleName = $ModuleName
+                ModuleRoot = $ModuleRoot
+            }
+        }
+
+        It -Name '<ModuleName> has a .psm1 file' {
             "$ModuleRoot\$ModuleName.psm1" | Should -Exist
         }
 
-        It 'has a .psd1 file' {
+        It -Name '<ModuleName> has a .psd1 file' {
             "$ModuleRoot\$ModuleName.psd1" | Should -Exist
         }
 
-        It "passes Test-ModuleManifest against manifest" {
+        It -Name "<ModuleName>.psd1 passes Test-ModuleManifest" {
             [bool](Test-ModuleManifest -Path "$ModuleRoot\$ModuleName.psd1") | Should -Be $true
         }
     }
 
     Context 'Function tests' {
-        It "<Name> should pass PSScriptAnalyzer" -TestCases @(
-            foreach ($Function in (Get-ChildItem "$ModuleRoot\Functions" -Recurse -Filter *.ps1)) {
+        It -Name "<Name> passes PSScriptAnalyzer" -TestCases @(
+            foreach ($Function in $Functions) {
                 @{
                     Name = $Function.Name
                     FullName = $Function.FullName
                 }
             }
         ) {
-            param(
-                $Name, $FullName
-            )
-
             @(Invoke-ScriptAnalyzer -Path $FullName) | Should -BeNullOrEmpty
         }
     }
